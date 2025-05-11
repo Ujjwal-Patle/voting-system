16a87da16a8d23c2238dbb5eec78166a4e802b92
Ujjwal-Patle <ujjwalpatle0@gmail.com>
2025-05-11 21:18:17 +0530

    add contact component to yout app.jsx

--- voting-system-ui/src/App.jsx
+++ voting-system-ui/src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
-import ContactPage from './component/contact';
+
 import './App.css'
 
 function App() {
 
 
   return (
-    <>
-      <ContactPage/ >
-    </>
+   
   )
 }
 
